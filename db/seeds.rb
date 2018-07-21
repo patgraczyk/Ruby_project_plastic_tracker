9beda8037b051f2f15c1f24a3c755f1941c860a8
Pat <graczyk.pat@gmail.com>
2018-07-21 11:41:19 +0100

    updated seeds file with tags data

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+require_relative("../models/tag.rb")
+require_relative("../models/plastic.rb")
+require_relative("../models/product.rb")
+require("pry-byebug")
+
+tag1 = Tag.new({
+  "category" => "cosmetics"
+  })
+
+tag1.save
+
+tag2 = Tag.new({
+  "category" => "hot_drink"
+  })
+
+tag2.save
+
+tag3 = Tag.new({
+  "category" => "plastic_bags"
+  })
+
+tag3.save 
