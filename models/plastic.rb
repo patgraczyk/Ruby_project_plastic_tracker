cb11356edb24f8696d5c031f50cdc84096c7d9f8
Pat <graczyk.pat@gmail.com>
2018-07-21 11:21:21 +0100

    plastic class file initialized

---
+++ models/plastic.rb
@@ -0,0 +1,12 @@
+require_relative( '../db/sql_runner' )
+
+class Plastic
+  attr_reader :id
+  attr_accessor :type, :existance, :weight
+
+def initialize( options )
+  @id = options['id'].to_i if options['id']
+  @type = options['type']
+  @existance = options['existance'].to_i
+  @weight = options['weight'].to_i
+end 
