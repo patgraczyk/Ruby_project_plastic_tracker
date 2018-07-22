f837380f3d7dfe171b74664383e5cd97a33b011f
Pat <graczyk.pat@gmail.com>
2018-07-22 13:58:34 +0100

    index page created and working

--- app.rb
+++ app.rb
@@ -4,9 +4,6 @@ require_relative('controllers/tags_controller')
 require_relative('controllers/plastics_controller')
 require_relative('controllers/products_controller')
 
-# get '/' do
-#   erb( :index )
-# end
-get '/index' do
-  "Hello World"
+get '/' do
+  erb( :index )
 end
