b2137d6e3be77304fe0f1e90190306e73f45617a
Pat <graczyk.pat@gmail.com>
2018-07-22 15:55:57 +0100

    links on tags fixed

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/tag.rb' )
+also_reload('./models/*')
 
-
-# get '/tags' do
-#   @tags = Tags.all()
-#   erb ( :"tags/index" )
-# end
+get '/tags' do
+  @tags = Tag.all()
+  erb ( :"tags/index" )
+end
