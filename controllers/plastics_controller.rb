17fa8343516b68769cef943dceafc04494b3a45f
Pat <graczyk.pat@gmail.com>
2018-07-22 16:19:42 +0100

    plastics route linked and working

--- controllers/plastics_controller.rb
+++ controllers/plastics_controller.rb
@@ -1,3 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/plastic.rb' )
+
+get '/plastics' do
+  @plastics = Plastic.all()
+  erb ( :"plastics/index" )
+end
