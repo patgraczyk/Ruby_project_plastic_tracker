52d494ec7b98639bbe1092d9d7894056ba5d5457
Pat <graczyk.pat@gmail.com>
2018-07-22 14:35:19 +0100

    more files added, first link does not work yet

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require( 'sinatra/contrib/all' )
 require_relative('controllers/tags_controller')
 require_relative('controllers/plastics_controller')
 require_relative('controllers/products_controller')
+also_reload('./models/*')
+
 
 get '/' do
   erb( :index )
--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -1,3 +1,9 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/tag.rb' )
+
+
+# get '/tags' do
+#   @tags = Tags.all()
+#   erb ( :"tags/index" )
+# end
