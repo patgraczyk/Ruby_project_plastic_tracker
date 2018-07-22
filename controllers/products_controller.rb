b2fc9f3dd041b2ef8045ed098552ba7dbab6c388
Pat <graczyk.pat@gmail.com>
2018-07-22 16:22:09 +0100

    products route linked and working

--- controllers/products_controller.rb
+++ controllers/products_controller.rb
@@ -4,3 +4,9 @@ require_relative( '../models/product.rb' )
 require_relative( '../models/plastic.rb' )
 require_relative( '../models/tag.rb' )
 require( 'pry-byebug' )
+
+
+get '/products' do
+  @products = Product.all()
+  erb ( :"products/index" )
+end
