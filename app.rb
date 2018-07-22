6c0856186d3367ab095c9a3b5f57b6074439cebd
Pat <graczyk.pat@gmail.com>
2018-07-22 13:51:13 +0100

    local host working and test 'get'

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require_relative('controllers/tags_controller')
 require_relative('controllers/plastics_controller')
 require_relative('controllers/products_controller')
 
-get '/' do
-  erb( :index )
+# get '/' do
+#   erb( :index )
+# end
+get '/index' do
+  "Hello World"
 end
