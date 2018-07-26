e2e175565362fbbdcbcff95e0564a80bce3103fe
Pat <graczyk.pat@gmail.com>
2018-07-26 20:18:46 +0100

    more styling added

--- models/tip.rb
+++ models/tip.rb
@@ -1,11 +1,11 @@
 class Tip
- 
+
   def self.all()
     tips = ["Use keep cup",
       "Refuse a straw",
-      "Use drawstring bag instead of a plastic one",
+      "Use drawstring bag",
       "Buy a water bottle",
-      "Get a soap in block instead of a bottle"]
+      ]
   end
 
 end #end of the class
