4204912ea57fafa999d8fd164e9ef1c6c1a88c66
Pat <graczyk.pat@gmail.com>
2018-08-03 16:00:10 +0100

    heroku setup

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+gem 'pg'
+ruby '2.4.1'
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,8 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'plastics_tracker', host: 'localhost'})
+      db = PG.connect({dbname: 'dek0j6qoir6n61', host: 'ec2-54-83-22-244.compute-1.amazonaws.com
+', port:5432, user:'faprqovlxwtqlh', password:'e59e430860d05e2b3bc2e0809e9c361bb9be33cf7e4d1a7fcef9cd5daca4bed6'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
